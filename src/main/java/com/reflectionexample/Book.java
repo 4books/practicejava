5e64670d426d0f603fb4355636d9ca0f9ee77325
4books <joonwoo9192@gmail.com>
2021-07-03 00:04:44 +0900

    test

---
+++ src/main/java/com/reflectionexample/Book.java
@@ -0,0 +1,9 @@
+package com.reflectionexample;
+
+public class Book {
+    private String a = "Book A";
+    private static String B = "Book B";
+    private static final String C = "Book C";
+    public String d = "Book D";
+    protected String e = "Book E";
+}
